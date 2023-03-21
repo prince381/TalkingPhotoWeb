fd93f838edbca7def63d32221448eb8ba07de6dc
Prince Owusu <princeowusu@Princes-MacBook-Pro.local>
2023-03-21 14:43:56 +0000

    fix: includes bug fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,6 @@ export default function Document() {
           rel='stylesheet'
           href='https://use.fontawesome.com/releases/v5.15.4/css/all.css'
           integrity='sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm'
-          crossOrigin='anonymous'
         ></link>
       </Head>
       <body className='w-screen overflow-x-hidden'>
