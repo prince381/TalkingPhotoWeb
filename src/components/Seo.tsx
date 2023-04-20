63d499baaf45237f95bd0b9053e0b53860a751ef
prince381 <powusu381@gmail.com>
2023-04-20 17:11:44 +0000

    feat: updated meta contents for seo

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -3,19 +3,19 @@ import { useRouter } from 'next/router';
 
 // !STARTERCONF Change these default meta
 const defaultMeta = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
-  siteName: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'All-In Podcasts',
+  siteName: 'All-In Podcasts',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+    'All-In Podcast is an AI platform that allows you to create and publish your own podcast using the All-In podcast besties.',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://allinpod.ai',
   type: 'website',
   robots: 'follow, index',
   /**
    * No need to be filled, will be populated with openGraph function
    * If you wish to use a normal image, just specify the path below
    */
-  image: 'https://tsnext-tw.thcl.dev/images/large-og.png',
+  image: 'https://allinpod.ai/images/allinpod.png',
 };
 
 type SeoProps = {
